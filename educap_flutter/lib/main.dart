5614a0aeffcb25e76ffd60ee89b47ace81db8876
Juan Chalita <chalajr2@gmail.com>
2021-11-11 16:45:31 -0600

    Little progress

--- educap_flutter/lib/main.dart
+++ educap_flutter/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Login());
+}
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
