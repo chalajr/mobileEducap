c2b61bbaab137dc1196f8a3bd0cca04b6294245f
Juan Chalita <chalajr2@gmail.com>
2021-11-11 18:40:25 -0600

    Develop layout

--- educap_flutter/lib/main.dart
+++ educap_flutter/lib/main.dart
@@ -1,121 +1,96 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Login());
+  runApp(const Layout());
 }
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+const eduCapBlue = Color(0xff5c8ec8);
+
+class Layout extends StatefulWidget {
+  const Layout({Key? key}) : super(key: key);
 
   @override
   _State createState() => _State();
 }
 
-class _State extends State<Login> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  static const eduCapBlue = Color(0xff5c8ec8);
-  // This widget is the root of your application.
+class _State extends State<Layout> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'EduCap'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const eduCapBlue = Color(0xff5c8ec8);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.white,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info_outline),
-            label: 'Sobre Nosotros',
-            backgroundColor: Colors.white,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Categorias',
-            backgroundColor: Colors.white,
-          ),
-        ],
         backgroundColor: eduCapBlue,
       ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Educap'),
+            backgroundColor: eduCapBlue,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            ),
+          ),
+          bottomNavigationBar: navBar()),
     );
   }
 }
 
-class SobreNosotros  extends StatefulWidget {
-  SobreNosotros ({ Key? key }) : super(key: key);
-
-  @override
-  SobreNosotrosState createState() => SobreNosotrosState();
+Widget navBar() {
+  return BottomNavigationBar(
+    type: BottomNavigationBarType.fixed,
+    selectedItemColor: Colors.white,
+    unselectedItemColor: Colors.white,
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.home,
+          color: Colors.white,
+        ),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.info_outline,
+          color: Colors.white,
+        ),
+        label: 'Sobre Nosotros',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.school,
+          color: Colors.white,
+        ),
+        label: 'Categorias',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.person,
+          color: Colors.white,
+        ),
+        label: 'Mi cuenta',
+      ),
+    ],
+    backgroundColor: eduCapBlue,
+  );
 }
 
-class SobreNosotrosState extends State<SobreNosotros> {
-  @override
-  Widget build(BuildContext context) {
-    return (
+
+
+
+// class SobreNosotros  extends StatefulWidget {
+//   SobreNosotros ({ Key? key }) : super(key: key);
+
+//   @override
+//   SobreNosotrosState createState() => SobreNosotrosState();
+
+// }
+// class SobreNosotrosState extends State<SobreNosotros> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return (
 
       
-    );
-  }
-}
+//     );
+//   }
+// }
